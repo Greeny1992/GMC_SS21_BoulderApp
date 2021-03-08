7fd414ddaf0d1e8c71b8ebc69e97a228eb440884
Georg Praßl <gprassl@dccsintra.net>
2021-03-08 13:49:52 +0100

    Updated App.tsx

--- BoulderApp/App.tsx
+++ BoulderApp/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hallo Theresa, Denis, Flo, Günther und Georg. Das wird ein mega Projekt =D </Text>
       <StatusBar style="auto" />
     </View>
   );
