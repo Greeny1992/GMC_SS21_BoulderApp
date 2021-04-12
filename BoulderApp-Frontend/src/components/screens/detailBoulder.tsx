b4c0353ffe0e7862b5bb7f0b3cac5e37fdc88d66
Georg Praßl <gprassl@dccsintra.net>
2021-04-12 20:41:13 +0200

    BugFix

--- BoulderApp-Frontend/src/components/screens/detailBoulder.tsx
+++ BoulderApp-Frontend/src/components/screens/detailBoulder.tsx
@@ -42,7 +42,6 @@ export default function DetailBoulder({route, navigation}: any) {
                         
                     </View>
                     <View
-                        style={styles.buttonContainer}
                     >
                     <Button
                         icon={{
--- BoulderApp-Frontend/src/styles/detailBoulder.tsx
+++ BoulderApp-Frontend/src/styles/detailBoulder.tsx
@@ -7,14 +7,6 @@ const DetailBoulderStyle = StyleSheet.create({
     },
     header: {
         width: '80%',
-    },
-    buttonContainer: {
-        width: '20%',
-        marginTop:10,
-    },
-    button:{
-        color: 'white',
-        border: 'none',
     }
 
 }
